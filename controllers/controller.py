ec9ac3ad35f0a0ab44cd03ccee458960bdef9c66
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-05 14:09:58 +0100

    MVP done

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,3 +10,14 @@ def index():
 @app.route('/events/new')
 def new_event():
     return render_template('new.html')
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    event_name = request.form['name']
+    event_date = request.form['date']
+    event_location = request.form['location']
+    event_guests = request.form['guests']
+    event_description = request.form['description']
+    new_event = Event(event_date, event_name, event_guests, event_location, event_description)
+    add_new_event(new_event)
+    return render_template('index.html', events = events)
