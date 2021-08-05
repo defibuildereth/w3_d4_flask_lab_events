ab5b36f2bcd71dd877507f4f68aab438f618971b
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-05 13:53:57 +0100

    can display events list

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,12 @@
 from models.event import Event
+from models.event_list import *
 from flask import render_template, request
 from app import app
+
+@app.route('/events')
+def index():
+    return render_template('index.html', events = events)
+
+@app.route('/events/new')
+def new_event():
+    return render_template('new.html')
